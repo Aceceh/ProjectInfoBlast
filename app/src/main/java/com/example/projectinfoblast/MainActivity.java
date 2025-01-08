5cc5cac0594c98154ceacef64ff72e44b2519926
ziqkim <haziqoverpower@gmail.com>
2025-01-09 01:59:12 +0800

    Haziq Tuto 13

--- app/src/main/java/com/example/projectinfoblast/MainActivity.java
+++ app/src/main/java/com/example/projectinfoblast/MainActivity.java
@@ -21,17 +21,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private RecyclerView recyclerView;
     private Toolbar mToolBar;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    private DataSnapshot dataSnapshot;
 
-// ADAM WAS HERE
+    // ADAM WAS HERE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +46,12 @@ public class MainActivity extends AppCompatActivity
 
         mAuth = FirebaseAuth.getInstance();
         mToolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
         setSupportActionBar(mToolBar); //add tool bar to main activity
         getSupportActionBar().setTitle("Home");
 
-
-
-
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.open_drawer,R.string.close_drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -68,10 +72,56 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item))
-        {
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null) {
+            SendUserToLoginActivity();
+        } else {
+            CheckUserExistence();
+        }
+    }
+
+    private void CheckUserExistence() {
+        final String current_user_id = mAuth.getCurrentUser().getUid();
+
+        UsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!dataSnapshot.hasChild(current_user_id)) {
+                    SendUserToSetupActivity();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+
+            }
+        });
+    }
+
+    private void SendUserToSetupActivity() {
+        Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupIntent);
+        finish();
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
@@ -86,17 +136,21 @@ public class MainActivity extends AppCompatActivity
         if (item.getItemId() == R.id.nav_addpost) {
             Toast.makeText(MainActivity.this, "Add New Post", Toast.LENGTH_SHORT).show();
 
-        }if (item.getItemId() == R.id.nav_home) {
+        }
+        if (item.getItemId() == R.id.nav_home) {
             Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
 
-        }if (item.getItemId() == R.id.nav_setting) {
+        }
+        if (item.getItemId() == R.id.nav_setting) {
             Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
 
-        }if (item.getItemId() == R.id.nav_logout) {
+        }
+        if (item.getItemId() == R.id.nav_logout) {
             mAuth.signOut();
             SendUserToLoginActivity();
         }
     }
+
     public boolean validateInputs(String email, String password) {
         if (email.isEmpty()) {
             Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show();
@@ -116,26 +170,9 @@ public class MainActivity extends AppCompatActivity
         }
         return true;
     }
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if(currentUser == null)
-        {
-            SendUserToLoginActivity();
-        }
-    }
-
-    private void SendUserToLoginActivity()
-    {
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(loginIntent);
-        finish();
-    }
 
 }
+
+
 
--- app/src/main/java/com/example/projectinfoblast/SetupActivity.java
+++ app/src/main/java/com/example/projectinfoblast/SetupActivity.java
@@ -1,11 +1,25 @@
 package com.example.projectinfoblast;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -14,6 +28,10 @@ public class SetupActivity extends AppCompatActivity {
     private EditText UserName, FullName, CountryName;
     private Button SaveInformationbuttion;
     private CircleImageView ProfileImage;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    private ProgressDialog loadingBar;
+    String currentUserID;
 
 
     @Override
@@ -22,17 +40,86 @@ public class SetupActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_setup);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+
         UserName = (EditText) findViewById(R.id.setup_username);
         FullName = (EditText) findViewById(R.id.setup_fullname);
         CountryName = (EditText) findViewById(R.id.setup_course);
-        SaveInformationbuttion = (Button) findViewById(R.id.register_button);
+        SaveInformationbuttion = (Button) findViewById(R.id.setup_button);
         ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
+        loadingBar = new ProgressDialog(this);
 
 
+        SaveInformationbuttion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                SaveAccountSetupInformation();
+            }
+        });
+    }
 
+    private void SaveAccountSetupInformation()
+    {
+        String username = UserName.getText().toString();
+        String fullname = FullName.getText().toString();
+        String country = CountryName.getText().toString();
 
+        if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(this, "Please enter your username...", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(fullname))
+        {
+            Toast.makeText(this, "Please enter your full name...", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(country))
+        {
+            Toast.makeText(this, "Please enter your course...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Saving Information");
+            loadingBar.setMessage("Please wait while we are creating your new account...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
 
+            HashMap userMap = new HashMap();
+            userMap.put("username", username);
+            userMap.put("fullname", fullname);
+            userMap.put("country", country);
+            userMap.put("status", "Hey there, i am using InfoBlast, developed by Captain America team!");
+            userMap.put("gender", "none");
+            userMap.put("dob", "");
+            userMap.put("relationshipstatus", "none");
+            UsersRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task)
+                {
+                    if (task.isSuccessful())
+                    {
+                        SendUserToMainActivity();
+                        Toast.makeText(SetupActivity.this, "Your account has been created successfully...", Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+                    }
+                    else
+                    {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Error occured : " + message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+    }
+}
 
-
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(SetupActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
